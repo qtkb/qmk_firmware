345b8403d0009b74dc1b158f7bd50b359fa66c06
qtkb <its.hip+qtkb@gmail.com>
2019-08-27 15:35:38 +0100

    Swap | and \

--- keyboards/thevankeyboards/minivan/keymaps/bnhw/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/bnhw/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------`------`------`------`------`------`------`------`------`------`------`-------------|
  * |          | 1    | 2    | 3    | 4    | 5    | 6    | 7    | 8    | 9    | 0    | `          |
  * |----------`------`------`------`------`------`------`------`------`------`------`------------|
- * |           | _    | -    | +    | =    | \    | |    | {    | [    | ]    | }    |           |
+ * |           | _    | -    | +    | =    | |    | \    | {    | [    | ]    | }    |           |
  * |-----------`------`------`------`------`-----'-------`------`------`------`------`-----------|
  * |         |         |         |                |                |         |         |         |
  *  `--------+---------+---------+------^^^-------+-----^^^--------+---------+---------+---------'
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_SY] = LAYOUT(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-    _______, KC_UNDS, KC_MINS, KC_PLUS, KC_EQL,  KC_BSLS, KC_PIPE, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, _______,
+    _______, KC_UNDS, KC_MINS, KC_PLUS, KC_EQL, KC_PIPE,  KC_BSLS, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, _______,
     _______, _______, _______,                   _______, _______,          _______, _______, _______
   ),
 /* Functions
