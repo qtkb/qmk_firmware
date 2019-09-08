dca09c2a8180a0d1b7b98d0bbdb6556637c7807a
qtkb <its.hip+qtkb@gmail.com>
2019-09-08 19:55:22 +0100

    Add OLED support

--- keyboards/qtkb/tsunami/keymaps/default/keymap.c
+++ keyboards/qtkb/tsunami/keymaps/default/keymap.c
@@ -16,6 +16,10 @@
 
 #include QMK_KEYBOARD_H
 
+#ifdef OLED_DRIVER_ENABLE
+extern bool oled_initialized;
+#endif
+
 // Layers
 enum
 {
@@ -45,3 +49,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
                                XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX), \
 };
+
+void matrix_scan_user(void) {
+  #ifdef OLED_DRIVER_ENABLE
+  if (!oled_initialized) {
+    oled_init(OLED_ROTATION_0);
+  }
+  #endif
+}
+
+void matrix_slave_scan_user(void)
+{
+  #ifdef OLED_DRIVER_ENABLE
+  if (!oled_initialized) {
+    oled_init(OLED_ROTATION_0);
+  }
+  #endif
+}
+
+#ifdef OLED_DRIVER_ENABLE
+static void render_logo(void) {
+  static const char PROGMEM logo[] = {
+    0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+    0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+    0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
+
+  oled_write_P(logo, false);
+}
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+  if (!is_keyboard_master())
+    return OLED_ROTATION_180;
+  return rotation;
+}
+
+void oled_task_user(void) {
+  render_logo();
+}
+#endif
