052a3dfbef39fd82dda00919b9f2fc7646dd818b
qtkb <its.hip+qtkb@gmail.com>
2020-01-08 13:13:42 +0000

    Update keymap to use keypad -+

--- keyboards/zinc/keymaps/bnhw/keymap.c
+++ keyboards/zinc/keymaps/bnhw/keymap.c
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
   [_SYMBOLS] = LAYOUT_ortho_4x12( \
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
       _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
-      _______, KC_UNDS, KC_MINS, KC_PLUS, KC_EQL,  KC_PIPE,                   KC_BSLS, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, _______, \
+      _______, KC_UNDS, KC_PMNS, KC_PPLS, KC_EQL,  KC_PIPE,                   KC_BSLS, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, _______, \
       _______, _______, _______, _______, _______, _______,                   _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END \
       ),
 
