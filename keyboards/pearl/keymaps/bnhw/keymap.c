67a15c668ae846fce95e22002d8c313d03bebfa7
qtkb <its.hip+qtkb@gmail.com>
2020-01-22 11:18:04 +0000

    Update keymap

--- keyboards/pearl/keymaps/bnhw/keymap.c
+++ keyboards/pearl/keymaps/bnhw/keymap.c
@@ -6,7 +6,6 @@ enum
     _SYMBOLS,
     _FUNCTIONS,
     _CONFIG,
-    _GAME,
 };
 
 // Tap dance
@@ -19,7 +18,6 @@ enum {
 #define SYM_ENT LT(_SYMBOLS, KC_ENT)
 #define FUNCS MO(_FUNCTIONS)
 #define CONFIG MO(_CONFIG)
-#define GAME MO(_GAME)
 #define C_TAB C(KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -27,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_BSPC,
     ESC_CTL,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     LS_CAPS,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                      KC_LGUI, KC_LALT, GAME,             KC_SPC,  SYM_ENT,          FUNCS,   CONFIG
+                      KC_LGUI, KC_LALT, KC_LCTL,          KC_SPC,  SYM_ENT,          FUNCS,   CONFIG
       ),
   [_SYMBOLS] = LAYOUT_splits(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  KC_DEL,
@@ -45,12 +43,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     _______,          RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     _______,          RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-                      _______, _______, _______,          _______, _______,          _______, _______
-  ),
-  [_GAME] = LAYOUT_splits(
-    C_TAB,   KC_1,    KC_2,    KC_3,    KC_4,    KC_F2,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    KC_F1,            KC_5,    KC_6,    KC_7,    KC_8,    KC_F3,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    _______,          KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_F4,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                       _______, _______, _______,          _______, _______,          _______, _______
   ),
 };
