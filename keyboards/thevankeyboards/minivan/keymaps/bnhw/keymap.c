9477a0aa89a43b36b15e4309ae5d2e8e90d2d2e3
qtkb <its.hip+qtkb@gmail.com>
2020-09-05 15:00:46 +0100

    Update minivan keymap

--- keyboards/thevankeyboards/minivan/keymaps/bnhw/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/bnhw/keymap.c
@@ -24,9 +24,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   [_QW] = LAYOUT(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_ESC,  KC_LGUI, KC_LALT,                   KC_SPC,  SY_ENT,           MO(_FN), XXXXXXX, XXXXXXX
+    KC_LGUI, KC_LALT, KC_LCTL,                   KC_SPC,  SY_ENT,           MO(_FN), XXXXXXX, XXXXXXX
   ),
 /* Symbols
  *
