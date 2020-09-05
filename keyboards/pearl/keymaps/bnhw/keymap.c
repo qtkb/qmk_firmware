0edacf10742e51ec341967a9df0cff2148ed47e1
qtkb <its.hip+qtkb@gmail.com>
2020-09-05 15:00:10 +0100

    Update pearl keymap

--- keyboards/pearl/keymaps/bnhw/keymap.c
+++ keyboards/pearl/keymaps/bnhw/keymap.c
@@ -30,51 +30,75 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_SYMBOLS] = LAYOUT_splits(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  KC_DEL,
     _______,          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-    _______,          KC_UNDS, KC_MINS, KC_PLUS, KC_EQL,  KC_PIPE, KC_BSLS, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, _______,
+    _______,          KC_UNDS, KC_PMNS, KC_PPLS, KC_EQL,  KC_PIPE, KC_BSLS, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, _______,
                       _______, _______, _______,          _______, _______,          _______, _______
   ),
   [_FUNCTIONS] = LAYOUT_splits(
     XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,
-    _______,          KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,
-    _______,          KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______,          KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_MPLY,
+    _______,          KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______,
                       _______, _______, _______,          _______, _______,          _______, _______
   ),
   [_CONFIG] = LAYOUT_splits(
     RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     _______,          RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    _______,          RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______,          RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, XXXXXXX, CG_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                       _______, _______, _______,          _______, _______,          _______, _______
   ),
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  // if we are on layer 1
-  if (state & (1<<1)){
-    // light num lock led
-    PORTD |= (1 << PD0);
-  }
-  else{
-    PORTD &= ~(1 << PD0);
-  }
-  // if we are on layer 2
-  if (state & (1<<2)){
-    // light caps lock led
-    PORTD |= (1 << PD1);
-  }
-  else{
-    PORTD &= ~(1 << PD1);
-  }
-  // if we are on layer 3
-  if (state & (1<<3)){
-    // light scroll lock led
-    PORTD |= (1 << PD6);
-  }
-  else{
-    PORTD &= ~(1 << PD6);
-  }
-    return state;
-  return state;
+void show_config_state_lights(bool swap_lctl_lgui)
+{
+    // Show left light if ctrl and gui swapped
+    if (swap_lctl_lgui)
+    {
+        PORTD |= (1 << PD0);
+    }
+    else
+    {
+        PORTD &= ~(1 << PD0);
+    }
 }
+
+void hide_lights(void)
+{
+    PORTD &= ~(1 << PD0);
+    PORTD &= ~(1 << PD1);
+    PORTD &= ~(1 << PD6);
+}
+
+layer_state_t layer_state_set_user(layer_state_t state)
+{
+    // Left:   (1 << PD0)
+    // Middle: (1 << PD1)
+    // Right:  (1 << PD6)
+    if (state & (1 << _CONFIG))
+    {
+        show_config_state_lights(keymap_config.swap_lctl_lgui);
+    }
+    // Disable lights by default
+    else
+    {
+        hide_lights();
+    }
+
+    return state;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record)
+{
+    switch (keycode)
+    {
+    case CG_TOGG:
+        if (record->event.pressed)
+        {
+            // Swap won't have happened yet
+            show_config_state_lights(!keymap_config.swap_lctl_lgui);
+        }
+        break;
+    }
+    return true;
+};
 
 typedef struct
 {
