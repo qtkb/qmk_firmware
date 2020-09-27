ac93102b40b6009b6e695d6fc8935b46403dc72b
qtkb <its.hip+qtkb@gmail.com>
2020-09-27 19:36:55 +0100

    tweak crkbd

--- keyboards/crkbd/keymaps/bnhw/keymap.c
+++ keyboards/crkbd/keymaps/bnhw/keymap.c
@@ -18,7 +18,7 @@ enum macro_keycodes {
 };
 
 #define FUN     MO(_FUN)
-#define ADJUST  MO(_ADJUST)
+#define GUI_ADJ LT(_ADJUST, KC_LGUI)
 #define SYM_ENT LT(_SYMBOL, KC_ENT)
 #define SNIP    S(G(KC_S))
 
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,     KC_O,   KC_P,    KC_BSPC,\
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,     KC_L,   KC_SCLN, KC_QUOT,\
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,\
-                                      KC_LALT, KC_LCTL, KC_SPC,  SYM_ENT, FUN,     ADJUST \
+                                      KC_LALT, KC_LCTL, KC_SPC,  SYM_ENT, FUN,     GUI_ADJ \
 ),
 [_SYMBOL] = LAYOUT_split_3x6_3( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
